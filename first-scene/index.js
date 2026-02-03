f4bd71e100e2e6fece05ef96f4ed768b564849cc
zlm <1104567294@qq.com>
2026-02-03 18:46:31 +0800

    Initial commit

---
+++ first-scene/index.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+import {
+    OrbitControls
+} from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+// 创建立方体
+{
+    const geometry = new THREE.BoxGeometry(100, 100, 100);
+    const material = new THREE.MeshLambertMaterial(({
+        color: new THREE.Color('orange')
+    }));
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(0, 0, 0);
+    scene.add(mesh);
+}
+// 创建光源
+{
+    const pointLight = new THREE.PointLight(0xffffff, 10000);
+    pointLight.position.set(80, 80, 80);
+    scene.add(pointLight);
+}
+//创建坐标系
+{
+    const axesHelper = new THREE.AxesHelper(200);
+    scene.add(axesHelper);
+}
+
+{
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+   
+    const camera = new THREE.PerspectiveCamera(60, width / height, 1, 1000);
+    camera.position.set(200, 200, 200);
+    camera.lookAt(0, 0, 0);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height)
+
+    function render() {
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    render();
+
+    document.body.append(renderer.domElement);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+}
